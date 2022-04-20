fe3910c23a2ef6ad3d98ee0e8f8313561ada9b45
RossikhinAV-home <rossikhinav@gmail.com>
2022-04-20 22:44:15 +0700

    1

--- ipscan.py
+++ ipscan.py
@@ -1,9 +1,9 @@
 import socket
 import sys
-mas = [20,21,22,23,25,42,43,53,67,69,80,110,115,123,137,138,139,143,161,179,443,445,514,515,993,5432]
+ports = [20,21,22,23,25,42,43,53,67,69,80,110,115,123,137,138,139,143,161,179,443,445,514,515,993,5432]
 host = input('Vvedite IP ')
 
-for port in mas:
+for port in ports:
   s = socket.socket()
   s.settimeout(1)
   try:
