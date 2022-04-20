316ffec7d65435633c31eba6795ec43b9639c075
RossikhinAV-home <rossikhinav@gmail.com>
2022-04-20 22:36:15 +0700

    20220420

---
+++ ipscan.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+mas = [20,21,22,23,25,42,43,53,67,69,80,110,115,123,137,138,139,143,161,179,443,445,514,515,993,5432]
+host = input('Vvedite IP ')
+
+for port in mas:
+  s = socket.socket()
+  s.settimeout(1)
+  try:
+    s.connect(host,port)
+  except socket.error:
+    pass
+  else:
+    s.close
+  print(host + ':' + str(port)+ 'active')
